d908fa13cd2372ad77597a2c44dcfccea2146430
sejin8839 <sejinon01@gmail.com>
2023-03-11 22:52:38 +0900

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,15 @@
+'''
+Date: Mar 11, 2023
+Author :  Name: SejinYoon
+          uID: u1311019
+Source code File name: problem1.py
+'''
+
+
+Num1 = input("Enter Num1: ")
+Num2 = input("Enter Num2: ")
+Num3 = input("Enter Num3: ")
+Num4 = input("Enter Num4: ")
+Num5 = input("Enter Num5: ")
+Average = (Num1+Num2+Num3+Num4+Num5) / 5
+print(Average)
