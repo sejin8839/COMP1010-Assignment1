70e52474f3f139b9132544f544064e75892368b6
sejin8839 <sejinon01@gmail.com>
2023-03-11 22:52:47 +0900

    Create problem2.py

---
+++ problem2.py
@@ -0,0 +1,12 @@
+'''
+Date: Mar 11, 2023
+Author :  Name: SejinYoon
+          uID: u1311019
+Source code File name: problem2.py
+'''
+
+
+Angle1 = input("Enter Angle1: ")
+Angle2 = input("Enter Angle2: ")
+Angle3 = 180-(Angle1+Angle2)
+print(Angle3)
