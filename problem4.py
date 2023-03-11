486f738d3ae033c46305e794346f799a13e21b32
sejin8839 <sejinon01@gmail.com>
2023-03-11 22:52:58 +0900

    Create problem4.py

---
+++ problem4.py
@@ -0,0 +1,15 @@
+'''
+Date: Mar 11, 2023
+Author :  Name: SejinYoon
+          uID: u1311019
+Source code File name: problem4.py
+'''
+
+
+Amount = 35*750
+Commission = Amount*0.02
+Total_Amount = Amount+Commission
+
+print(f'{Amount}$')
+print(f'{Commission}$')
+print(f'{Total_Amount}$')
